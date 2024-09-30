e8e0a4e8e26413286e1b16d26f3a1f3dff784412
qnnn <65326092+qnnn@users.noreply.github.com>
2024-09-30 23:00:29 +0800

    Add unit tests for body caching in CircuitBreakerGatewayFilter.

--- spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/SpringCloudCircuitBreakerFilterFactoryTests.java
+++ spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/filter/factory/SpringCloudCircuitBreakerFilterFactoryTests.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 import org.springframework.cloud.gateway.test.BaseWebClientTests;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -136,6 +137,13 @@ public abstract class SpringCloudCircuitBreakerFilterFactoryTests extends BaseWe
 		testClient.get().uri("/status/200").header("Host", "www.circuitbreakerresumewithouterror.org").exchange()
 				.expectStatus().isOk().expectHeader()
 				.valueEquals(ROUTE_ID_HEADER, "circuitbreaker_resume_without_error");
+	}
+
+	@Test
+	public void filterPostFallback() {
+		testClient.post().uri("/post").body(BodyInserters.fromValue("hello"))
+				.header("Host", "www.circuitbreakerfallbackpost.org").exchange().expectStatus()
+				.isOk().expectBody().json("{\"body\":\"hello\"}");
 	}
 
 }
